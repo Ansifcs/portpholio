3e76ca02e5acf293f47fd5afdc5a7f3ad7a0a181
Ansifcs <ansifshanu0@gmail.com>
2026-07-12 15:27:30 +0530

    feat: add Skills component to display programming skills in the portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,48 @@
 import { MoveUpRight, Sparkle } from "lucide-react";
 
+const skills = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Python",
+  "mongodb",
+  "Express.js",
+  "HTML",
+  "CSS",
+  "Tailwind CSS",
+  "Git",
+  "GitHub",
+];
+
+function Skills() {
+
+
+  
+  return (
+    <div className="bg-gray-900 flex gap-2">
+
+      <div className="w-[15%] text-7xl uppercase font-extrabold">
+        <span>skills</span>
+        <div>
+          <span className="text-lime-200">i use</span>
+        </div>
+      </div>
+
+      <div className="w-full px-2 flex gap-4 flex-wrap items-center justify-center uppercase font-bold">
+        {skills.map((skill) => (
+          <div key={skill} className="text-2xl text-gray-300 border-2 border-lime-400 rounded-lg py-2 px-4 flex gap-2 items-center">
+            <div className="bg-lime-400 w-2 h-2 rounded-full"/>
+            <span>{skill}</span>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div>
@@ -20,6 +63,8 @@ export default function Home() {
           <MoveUpRight size={120} />
         </div>
       </div>
+
+      <Skills />
     </div>
   );
 }
